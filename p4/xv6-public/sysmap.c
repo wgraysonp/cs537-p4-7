c1ebd1362cb27c3c00fabe99ba22053af88533f5
wgraysonp <gpow1993@gmail.com>
2024-03-02 15:05:45 -0600

    added  file for memory mapping system calls

---
+++ p4/xv6-public/sysmap.c
@@ -0,0 +1,19 @@
+// Memory mapping system calls
+
+
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "mmu.h"
+#include "proc.h"
+#include "fs.h"
+#include "file.h"
+#include "wmap.h"
+
+int sys_wmap(void){
+	return 0;
+}
+
+int sys_wunmap(void){
+	return 0;
+}
